20384a760ce71655416c5a6e465c16d957fffca5
Amanda Lind Jonsson <amandizz_96@hotmail.com>
2021-12-02 12:22:55 +0100

    Tagit bort från main och lagt i ny fil

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'add_view.dart';
 import 'model.dart';
 import 'todo_list.dart';
+import 'todolist_view.dart';
 
 void main() {
   var state = MyState();
@@ -29,58 +30,5 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-        actions: [
-          PopupMenuButton(
-              onSelected: (String value) => {
-                    Provider.of<MyState>(context, listen: false)
-                        .setFilterBy(value)
-                  },
-              itemBuilder: (context) => [
-                    PopupMenuItem(child: Text('Done'), value: 'Done'),
-                    PopupMenuItem(child: Text('Undone'), value: 'Undone'),
-                    PopupMenuItem(child: Text('All'), value: 'All'),
-                  ]),
-        ],
-      ),
-      body: Consumer<MyState>(
-        builder: (context, state, child) =>
-            TodoList(_filterList(state.list, state.filterBy)),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () async {
-            var newTodoTitle = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => AddView(TodoCheckboxes(
-                          title: '',
-                          id: '',
-                        ))));
-            if (newTodoTitle != null) {
-              Provider.of<MyState>(context, listen: false)
-                  .addTodo(newTodoTitle);
-            }
-          }),
-    );
-  }
-
-  List<TodoCheckboxes> _filterList(list, filterBy) {
-    if (filterBy == "Done") {
-      return list.where((checkbox) => checkbox.done == true).toList();
-    }
-    if (filterBy == "Undone") {
-      return list.where((checkbox) => checkbox.done == false).toList();
-    }
-    return list;
-  }
+  State<MyHomePage> createState() => MyHomePageState();
 }
